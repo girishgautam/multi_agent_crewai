348e459008dbb7e41ee2dc799234039a7d300a28
girish joshi <girishgautam@yahoo.com>
2024-06-18 17:15:33 +0100

    new notebook- advanced agent collaboration for financial analysis

--- utils.py
+++ utils.py
@@ -8,3 +8,9 @@ def get_openai_api_key():
     load_env()
     openai_api_key = os.getenv("OPENAI_API_KEY")
     return openai_api_key
+
+
+def get_serper_api_key():
+    load_env()
+    openai_api_key = os.getenv("SERPER_API_KEY")
+    return openai_api_key
